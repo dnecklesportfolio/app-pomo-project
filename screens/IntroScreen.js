aeb3010588839ec4a914b3c868c8092e65bbca07
Dwayne Neckles <dwayneaneckles@gmail.com>
2017-05-08 17:18:47 -0400

    created rough versioning of sign in screen w button

--- screens/IntroScreen.js
+++ screens/IntroScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import { Button } from 'react-native-elements';
 import styled from 'styled-components/native';
 import tachyons from 'styled-components-tachyons';
 
@@ -8,12 +8,26 @@ export default  class IntroScreen extends React.Component {
   static navigationOptions = {
     title: 'Welcome',
   };
+  onIntroButtonPress = () => {
+    this.props.navigation.navigate('login');
+  }
   render() {
     return (
      <View style={styles.container}>
 
-        <Text>Intro screentetst</Text>
-              
+        <Text>Welcome
+to PomoTime</Text>
+
+<Text>You will sign in through fb
+first, then once logged in
+tap any of the timer
+to configure them.</Text>
+         <Button
+  raised
+  buttonStyle={{backgroundColor: 'gray', borderRadius: 5}}
+  textStyle={{textAlign: 'center'}}
+  title={"SIgn In!"}
+  onPress={this.onIntroButtonPress} />     
   </View>
     );
   }
